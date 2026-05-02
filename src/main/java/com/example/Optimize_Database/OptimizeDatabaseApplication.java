0a554d5b1d48595545c0c77d2184f120448a0fd7
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-05-02 13:43:22 +0600

    init

---
+++ src/main/java/com/example/Optimize_Database/OptimizeDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.Optimize_Database;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptimizeDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OptimizeDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Optimize_Database/OptimizeDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Optimize_Database;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OptimizeDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
