70bbad7f133e9b310e7d9c34bf7e6e9838bc9fd6
Faiyaz502 <www.fahim.faizas@gmail.com>
2026-05-07 19:29:11 +0600

    Database migration

--- src/main/java/com/example/Optimize_Database/CQRS/multipleDbManagement/DataSourceAspect.java
+++ src/main/java/com/example/Optimize_Database/CQRS/multipleDbManagement/DataSourceAspect.java
@@ -1,25 +1,25 @@
-package com.example.Optimize_Database.CQRS.multipleDbManagement;
-
-import org.aspectj.lang.ProceedingJoinPoint;
-import org.aspectj.lang.annotation.Around;
-import org.aspectj.lang.annotation.Aspect;
-import org.springframework.core.annotation.Order;
-import org.springframework.stereotype.Component;
-
-@Aspect
-@Component
-@Order(0)       // Must run BEFORE @Transactional
-public class DataSourceAspect {
-
-    @Around("@annotation(com.hotel.service.HotelService.multipleDbManagement.ReadOnly)")
-    public Object proceed(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
-        try {
-            // Switch to the 3 Replicas
-            DataSourceContextHolder.set("READ");
-            return proceedingJoinPoint.proceed();
-        } finally {
-            // Always clear to prevent memory leaks and defaulting to Primary
-            DataSourceContextHolder.clear();
-        }
-    }
-}
+//package com.example.Optimize_Database.CQRS.multipleDbManagement;
+//
+//import org.aspectj.lang.ProceedingJoinPoint;
+//import org.aspectj.lang.annotation.Around;
+//import org.aspectj.lang.annotation.Aspect;
+//import org.springframework.core.annotation.Order;
+//import org.springframework.stereotype.Component;
+//
+//@Aspect
+//@Component
+//@Order(0)       // Must run BEFORE @Transactional
+//public class DataSourceAspect {
+//
+//    @Around("@annotation(com.hotel.service.HotelService.multipleDbManagement.ReadOnly)")
+//    public Object proceed(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+//        try {
+//            // Switch to the 3 Replicas
+//            DataSourceContextHolder.set("READ");
+//            return proceedingJoinPoint.proceed();
+//        } finally {
+//            // Always clear to prevent memory leaks and defaulting to Primary
+//            DataSourceContextHolder.clear();
+//        }
+//    }
+//}
